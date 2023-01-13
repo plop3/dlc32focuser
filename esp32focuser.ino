f630f163ee8911f5afdd6cacb23cafbf08043b17
Serge <serge.claus@free.fr>
2023-01-13 18:34:30 +0100

    Mise au propre

--- esp32focuser.ino
+++ esp32focuser.ino
@@ -2,6 +2,10 @@
 ESP32 focuser, dew-heater for MKS DLC32
 */
 
+/////////////////////////////////////////////////////
+// Configuration //
+#define VIT 2000        // µs/pas
+
 // Pinout
 #define dataPin  21
 #define clockPin 16
@@ -11,66 +15,62 @@ ESP32 focuser, dew-heater for MKS DLC32
 #define stepPin  1
 #define heat2Pin 3
 #define interPin 4
-
-byte data=0;  // State of EN, step, dir...)
-
-// Etat moteur
-
-#define VIT 2000
-
-static volatile bool Depl=false;
-static volatile int Distance;
+/////////////////////////////////////////////////////
+// Variables globales //
+byte data=0;                               // Etat de  EN, step, dir... (74HC595)
+unsigned int Position;                     // Position du focuser
+bool Sens=true;                            // Sens de rotation
+static volatile bool MotMove=false;        // Moteur en mouvement 
+static volatile unsigned int MotDist;      // Distance à parcourir
 
 hw_timer_t *Tmot = NULL;
 
+/////////////////////////////////////////////////////
+// Fonctions rapides //
 void IRAM_ATTR motorRun() {
-  if (Distance == 0) {
+  if (MotDist == 0) {
     timerAlarmDisable(Tmot);
     digitalWrite2(enPin, HIGH);
-    Depl=false;
+    MotMove=false;
   }
   else {
       digitalWrite2(stepPin, HIGH);
       digitalWrite2(stepPin, LOW);
-      Distance--;
+      MotDist--;
   }
 }
 
+/////////////////////////////////////////////////////
+// Setup, Loop //
 void setup() {
   Serial.begin(115200);
+  // Initialisation 74HC595
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  // Timer moteur
   Tmot=timerBegin(0,80,true);
   timerAttachInterrupt(Tmot, &motorRun, true);
   timerAlarmWrite(Tmot,VIT,true);
-  //timerStop(Tmot);
-  Serial.println("Setup");
 }
 
-bool Sens=true; 
-
 void loop() {
-  if (!Depl) {
-    Serial.println("Debut");
+  if (!MotMove) {
     delay(2000);
-    Serial.println("Apres 2s");
     Sens=!Sens;
     bouge(15000,Sens);
-    Serial.println("Fin deplacement");
-    delay(1000);
-    Serial.println("Apres 1s");
-    Serial.println("***");
   }
 }
 
-void bouge(int Dist, bool Sens) {
+/////////////////////////////////////////////////////
+// Fonctions //
+void bouge(unsigned int Dist, bool Sens) {
   //timerRestart(Tmot);
   timerAlarmEnable(Tmot);
   digitalWrite2(dirPin, (Sens)?HIGH:LOW); // Enable stepper
-  Distance=Dist;
+  MotDist=Dist;
     digitalWrite2(enPin, LOW); // Enable stepper
-    Depl=true;
+    MotMove=true;
 }
 
 void digitalWrite2(uint8_t PIN, uint8_t STATE) {
@@ -79,3 +79,5 @@ void digitalWrite2(uint8_t PIN, uint8_t STATE) {
   shiftOut(dataPin, clockPin, MSBFIRST, data);
   digitalWrite(latchPin, HIGH);
 }
+
+/////////////////////////////////////////////////////
